1e6d22798950b90d8877394850a731a95686be6a
Emily Alfs-Votipka <emilyalfs@ksu.edu>
2023-12-28 12:54:28 -0600

    v2 of rat train

--- exps/example/marblerats/marblerats_train.py
+++ exps/example/marblerats/marblerats_train.py
@@ -1,126 +1,45 @@
-# encoding: utf-8
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
 import os
-import random
-import torch
-import torch.nn as nn
-import torch.distributed as dist
 
 from yolox.exp import Exp as MyExp
-from yolox.data import get_yolox_datadir
+from torch.utils.data import Dataset, DataLoader
 
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.num_classes = 80
         self.depth = 1.33
         self.width = 1.25
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        self.train_ann = "train.json"
-        self.test_ann = "test.json"
-        self.val_ann = "val.json"
-        self.input_size = (800, 1440)
-        self.test_size = (800, 1440)
-        self.random_size = (18, 32)
-        self.max_epoch = 80
-        self.print_interval = 20
-        self.eval_interval = 5
-        self.test_conf = 0.1
-        self.nmsthre = 0.7
-        self.no_aug_epochs = 10
-        self.basic_lr_per_img = 0.001 / 64.0
-        self.warmup_epochs = 1
+
 
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
-        from yolox.data import (
-            MOTDataset,
-            TrainTransform,
-            YoloBatchSampler,
-            DataLoader,
-            InfiniteSampler,
-            MosaicDetection,
-        )
 
-        dataset = MOTDataset(
-            data_dir=os.path.join(get_yolox_datadir(), "marblerats"),
-            json_file=self.train_ann,
+        dataset = Dataset(
+            data_dir=os.path.join("/content/OC_SORT/datasets/", "marblerats"),
+            json_file="/content/OC_SORT/datasets/marblerats/annotations/train.json",
             name='train',
-            img_size=self.input_size,
-            preproc=TrainTransform(
-                rgb_means=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-                max_labels=500,
-            ),
-        )
-
-        dataset = MosaicDetection(
-            dataset,
-            mosaic=not no_aug,
-            img_size=self.input_size,
-            preproc=TrainTransform(
-                rgb_means=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-                max_labels=1000,
-            ),
-            degrees=self.degrees,
-            translate=self.translate,
-            scale=self.scale,
-            shear=self.shear,
-            perspective=self.perspective,
-            enable_mixup=self.enable_mixup,
+            img_size=self.input_size
         )
 
         self.dataset = dataset
 
-        if is_distributed:
-            batch_size = batch_size // dist.get_world_size()
-
-        sampler = InfiniteSampler(
-            len(self.dataset), seed=self.seed if self.seed else 0
-        )
-
-        batch_sampler = YoloBatchSampler(
-            sampler=sampler,
-            batch_size=batch_size,
-            drop_last=False,
-            input_dimension=self.input_size,
-            mosaic=not no_aug,
-        )
-
-        dataloader_kwargs = {"num_workers": self.data_num_workers, "pin_memory": True}
-        dataloader_kwargs["batch_sampler"] = batch_sampler
-        train_loader = DataLoader(self.dataset, **dataloader_kwargs)
+        train_loader = DataLoader(self.dataset)
 
         return train_loader
 
     def get_eval_loader(self, batch_size, is_distributed, testdev=False):
-        from yolox.data import MOTDataset, ValTransform
 
-        valdataset = MOTDataset(
-            data_dir=os.path.join(get_yolox_datadir(), "marblerats"),
-            json_file=self.val_ann,
+        valdataset = Dataset(
+            data_dir=os.path.join("/content/OC_SORT/datasets/", "marblerats"),
+            json_file="/content/OC_SORT/datasets/marblerats/annotations/val.json",
             img_size=self.test_size,
-            name='train',
-            preproc=ValTransform(
-                rgb_means=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-            ),
+            name='val'
         )
 
-        if is_distributed:
-            batch_size = batch_size // dist.get_world_size()
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                valdataset, shuffle=False
-            )
-        else:
-            sampler = torch.utils.data.SequentialSampler(valdataset)
-
-        dataloader_kwargs = {
-            "num_workers": self.data_num_workers,
-            "pin_memory": True,
-            "sampler": sampler,
-        }
-        dataloader_kwargs["batch_size"] = batch_size
-        val_loader = torch.utils.data.DataLoader(valdataset, **dataloader_kwargs)
+        val_loader = DataLoader(valdataset)
 
         return val_loader
 
